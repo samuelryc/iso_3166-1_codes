e2531ca35e64dab085084c9ef9f6da55a7abf224
Samuel Ryc <contact@samuelryc.com>
2021-03-28 17:16:24 -0700

    Update and rename iso_3166-1_alpha-2.sql to two-letter_country_codes.sql

---
+++ two-letter_country_codes.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS two-letter_country_codes (
+    id SMALLINT NOT NULL,
+    code VARCHAR(2) NOT NULL,
+    country VARCHAR(40) NOT NULL
+    PRIMARY KEY (id)
+);
+
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (20, 'AD', 'Andorra');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (784, 'AE', 'United Arab Emirates');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (28, 'AG', 'Antigua and Barbuda');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (660, 'AI', 'Anguilla');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (8, 'AL', 'Albania');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (51, 'AM', 'Armenia');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (24, 'AO', 'Angola');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (10, 'AQ', 'Antarctica');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (32, 'AR', 'Argentina');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (16, 'AS', 'American Samoa');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (40, 'AT', 'Austria');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (36, 'AU', 'Australia');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (533, 'AW', 'Aruba');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (248, 'AX', 'Åland Islands');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (31, 'AZ', 'Azerbaijan');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (70, 'BA', 'Bosnia and Herzegovina');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (52, 'BB', 'Barbados');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (50, 'BD', 'Bangladesh');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (56, 'BE', 'Belgium');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (854, 'BF', 'Burkina Faso');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (100, 'BG', 'Bulgaria');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (48, 'BH', 'Bahrain');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (108, 'BI', 'Burundi');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (204, 'BJ', 'Benin');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (204, 'BJ', 'Saint Barthélemy');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (60, 'BM', 'Bermuda');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (96, 'BN', 'Brunei Darussalam');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (68, 'BO', 'Bolivia, Plurinational State of');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (535, 'BQ', 'Bonaire, Sint Eustatius and Saba');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (76, 'BR', 'Brazil');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (44, 'BS', 'Bahamas');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (64, 'BT', 'Bhutan');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (74, 'BV', 'Bouvet Island');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (72, 'BW', 'Botswana');
+INSERT INTO two-letter_country_codes(id, code, country) VALUES (72, 'BW', 'Botswana');
