c72a61a05357e4ada55adad4b651871202e87a61
Samuel Ryc <contact@samuelryc.com>
2021-06-06 12:38:12 -0400

    add numerci codes for php and python and started sql + fix sql three letter country codes

--- data/three-letter_country_codes/three-letter_country_codes.sql
+++ data/three-letter_country_codes/three-letter_country_codes.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS country_codes (
     id SMALLINT NOT NULL,
-    code VARCHAR(2) NOT NULL,
+    code VARCHAR(3) NOT NULL,
     country VARCHAR(52) NOT NULL,
     PRIMARY KEY (id)
 );
